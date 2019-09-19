9c6e4af7bc5234786bc2615d0ca5aa312eb35ad3
Paulo Baptista <paulofbaptista@gmail.com>
2019-09-19 13:12:38 +0100

    Add temporary contact info to index page
    Added temporary content to the index page.

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <h1>Paulo Baptista</h1>
+        <p>Nothing to see here... yet! Check back soon!</p>
+        <h2>Get in touch</h2>
+        <p>Meanwhile you can follow me on Twitter or email me at paulo [at] paulobaptista.dev</p>
+    </div>
+)
