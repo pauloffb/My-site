aa7666358b5ff9e7bcc7c7eaddea1b9149c0b6a4
Paulo Baptista <paulofbaptista@gmail.com>
2019-09-20 16:58:49 +0100

    Edit index page temporary information

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default () => (
-    <div>
-        <h1>Paulo Baptista</h1>
-        <p>Nothing to see here... yet! Check back soon!</p>
-        <h2>Get in touch</h2>
-        <p>Meanwhile you can follow me on Twitter or email me at paulo [at] paulobaptista.dev</p>
+  <div className="h-screen flex flex-col items-center justify-center text-center p-2">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Paulo Baptista</title>
+    </Helmet>
+    <div className="">
+      <h1 className="text-4xl md:text-6xl">Paulo Baptista</h1>
+      <p className="font-body">Nothing to see here... yet! Check back soon!</p>
+      <h2 className="text-2xl md:text-4xl mt-8">Get in touch</h2>
+      <p>
+        You can follow me on Twitter or email me at paulo [at] paulobaptista.dev
+      </p>
     </div>
+  </div>
 )
