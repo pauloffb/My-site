13a588eec4e2229b2499d1499122f62d74fc1099
Paulo Baptista <paulofbaptista@gmail.com>
2019-09-23 18:21:28 +0100

    Add logo, add responsive text
    Added logo, and made some changes to have text scaling according to
    viewport size

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,16 +1,20 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import logo from "../images/icon.png"
 
 export default () => (
-  <div className="h-screen flex flex-col items-center justify-center text-center p-2">
+  <div className="h-screen flex flex-col items-center justify-center text-center p-2 border-t-4 sm:border-t-8 border-red-600">
     <Helmet>
       <meta charSet="utf-8" />
       <title>Paulo Baptista</title>
     </Helmet>
-    <div className="">
-      <h1 className="text-4xl md:text-6xl">Paulo Baptista</h1>
+    <div className="flex flex-row items-center">
+      <img className="h-10 md:h-20 mr-2" src={logo} alt="Paulo Baptista logo" />
+      <h1 className="text-2xl sm:text-4xl md:text-6xl">Paulo Baptista</h1>
+    </div>
+    <div>
       <p className="font-body">Nothing to see here... yet! Check back soon!</p>
-      <h2 className="text-2xl md:text-4xl mt-8">Get in touch</h2>
+      <h2 className="text-xl sm:text-2xl md:text-4xl mt-8">Get in touch</h2>
       <p>
         You can follow me on Twitter or email me at paulo [at] paulobaptista.dev
       </p>
