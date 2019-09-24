6bc08521b40f7ef84f18adeb9b2b7a3deba2de08
Paulo Baptista <paulofbaptista@gmail.com>
2019-09-24 15:21:22 +0100

    Responsive tweaks temporary homepage

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -8,15 +8,15 @@ export default () => (
       <meta charSet="utf-8" />
       <title>Paulo Baptista</title>
     </Helmet>
-    <div className="flex flex-row items-center">
-      <img className="h-10 md:h-20 mr-2" src={logo} alt="Paulo Baptista logo" />
+    <div className="flex flex-col sm:flex-row items-center">
+      <img className="h-16 mb-8 sm:mb-0 md:h-20 sm:mr-2" src={logo} alt="Paulo Baptista logo" />
       <h1 className="text-2xl sm:text-4xl md:text-6xl">Paulo Baptista</h1>
     </div>
     <div>
       <p className="font-body">Nothing to see here... yet! Check back soon!</p>
       <h2 className="text-xl sm:text-2xl md:text-4xl mt-8">Get in touch</h2>
       <p>
-        You can follow me on Twitter or email me at paulo [at] paulobaptista.dev
+        You can follow me on <a class="underline text-blue-500 hover:text-red-600 font-semibold " href="https://twitter.com/pauloffb" target="_blank" rel="noopener noreferrer">Twitter</a> or email me at paulo [at] paulobaptista.dev
       </p>
     </div>
   </div>
